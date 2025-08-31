d1503c050a5d4fa3c02689ac2b671e30b6fe595e
wish13yt <100239673+wish13yt@users.noreply.github.com>
2025-08-31 07:00:07 -0500

    add temp

--- src/food.py
+++ src/food.py
@@ -1,7 +1,20 @@
 import argparse
 import urllib.request
+import os
 parser = argparse.ArgumentParser(prog='Food', description='A package manager in Python')
-parser.add_argument('--i', '--install') # install + package
+parser.add_argument('--i', '--install') # install + package (ex: --i greg)
+parser.add_argument('--t', '--temp') # download temp then delete
 args = parser.parse_args()
 if (args.i == "greg"):
-    urllib.request.urlretrieve("", args.i + ".py")
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/Freakybob-Team/food/refs/heads/main/packages/greg.py", args.i + ".py")
+if 'args.i' in globals():
+    with open(args.i + ".py") as file:
+        exec(file.read())
+if (args.t == "greg"):
+    urllib.request.urlretrieve("https://raw.githubusercontent.com/Freakybob-Team/food/refs/heads/main/packages/greg.py", args.t + ".py")
+with open(args.t + ".py") as file:
+    exec(file.read())
+    try:
+        os.remove(args.t + ".py")
+    except:
+        print("failed to delete file")
