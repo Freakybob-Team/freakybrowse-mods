a9a00827eb68983388d41973aac08706c3c2ebce
wish13yt <100239673+wish13yt@users.noreply.github.com>
2025-09-02 17:47:55 -0500

    change type system

--- src/food.py
+++ src/food.py
@@ -1,18 +1,19 @@
 import argparse
 import eat
 helpstring = """
-here's an example of downloading a python app: python3 food.py --i greg --py yes
-here's an example of downloading a text file: python3 food.py --i greg --txt yes
-formatting: [your python name, ex: python3 or py] food.py --i [packagename] --[type] [anything here, --type just needs something after it]"""
+here's an example of downloading a python app: python3 food.py --i greg --type text
+here's an example of downloading a text file: python3 food.py --i greg --type python
+formatting: [your python name, ex: python3 or py] food.py --i [packagename] --[type] [python or text]"""
 parser = argparse.ArgumentParser(prog='Food', description='A package manager in Python', epilog=helpstring)
 parser.add_argument('--i')
 #parser.add_argument('--t') - coming soon once again
-parser.add_argument('--txt')
-parser.add_argument('--py')
+parser.add_argument('--type')
 args = parser.parse_args()
 if args.py:
-    print("using python downloads")
-    eat.python(args)
+    if args.type == "python":
+        print("using python downloads")
+        eat.python(args)
 if args.txt:
-    print("using txt downloads")
-    eat.txt(args)
+    if args.type == "text":
+        print("using txt downloads")
+        eat.txt(args)
