df6c2858c4c3882c03b3d734114b2a7d4ef29595
wish13yt <100239673+wish13yt@users.noreply.github.com>
2025-09-15 04:45:43 -0500

    greg

--- src/eat.py
+++ src/eat.py
@@ -5,36 +5,18 @@ import sys
 
 
 def python(args):
-    with urllib.request.urlopen("https://food.freakybob.site/autumn/food.json") as url:
+    with urllib.request.urlopen("https://freakybob-team.github.io/freakybrowse-mods/autumn/food.json") as url:
         data = json.load(url)
         print("Packages fetched! [autumn, food.json]")
     if (args.i in data["pypackages"]):
         print("Package found!")
         urllib.request.urlretrieve(
-            "https://food.freakybob.site/packages/python/" + args.i + ".py", args.i + ".py"
+            "https://freakybob-team.github.io/freakybrowse-mods/packages/python/" + args.i + ".py", args.i + ".py"
         )
         print("Package downloaded!")
         print("The package will now run.")
         print("-----")
         with open(args.i + ".py") as file:
             exec(file.read())
-    else:
-        sys.exit("Package not found. Food will now exit.")
-
-
-def txt(args):
-    with urllib.request.urlopen("https://food.freakybob.site/autumn/food.json") as url:
-        data = json.load(url)
-        print("Packages fetched! [autumn, food.json]")
-    if (args.i in data["txt"]):
-        print("Package found!")
-        urllib.request.urlretrieve(
-            "https://food.freakybob.site/packages/txt/" + args.i + ".txt", args.i + ".txt"
-        )
-        print("Package downloaded!")
-        print("The package will now run.")
-        print("-----")
-        with open(args.i + ".txt") as file:
-            print(file.read())
     else:
         sys.exit("Package not found. Food will now exit.")
--- src/food.py
+++ src/food.py
@@ -1,21 +1,13 @@
 import argparse
 import eat
-import cook
 helpstring = """
 here's an example of downloading a python app: python3 food.py --i greg --type text
 here's an example of downloading a text file: python3 food.py --i greg --type python
 formatting: [your python name, ex: python3 or py] food.py --i [packagename] --[type] [python or text]"""
-parser = argparse.ArgumentParser(prog='Food', description='A package manager in Python', epilog=helpstring)
+parser = argparse.ArgumentParser(prog='FreakybrowseMods', description='Mods for Freakybrowse', epilog=helpstring)
 parser.add_argument('--i')
-parser.add_argument('--t')
 parser.add_argument('--type')
 args = parser.parse_args()
 if args.type == "python":
     print("using python downloads")
     eat.python(args)
-if args.type == "text":
-    print("using txt downloads")
-    eat.txt(args)
-if args.t:
-    print("using temp downloads; python")
-    cook.cookpy(args)
